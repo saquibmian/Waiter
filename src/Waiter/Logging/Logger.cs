bfd899619894d5f7b45cb482a9f768396bc99c05
Saquib Mian <saquib.mian@gmail.com>
2013-12-12 21:09:48 -0500

    + Adding Error logging

--- src/Waiter/Logging/Logger.cs
+++ src/Waiter/Logging/Logger.cs
@@ -5,6 +5,13 @@ namespace Waiter.Logging {
         
         internal static void Info( string msg, params object[] args ) {
             Console.WriteLine( "INFO: {0}", string.Format( msg, args ) );
+        }          
+        
+        internal static void Error( string msg, params object[] args ) {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine( "ERROR: {0}", string.Format( msg, args ) );
+            Console.ForegroundColor = previousColor;
         }    
     }
 }
