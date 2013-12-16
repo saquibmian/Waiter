9280e231b3369ed4df5d31b04b7e7ab2fd097213
Saquib Mian <saquib.mian@gmail.com>
2013-12-15 21:46:25 -0500

    * Refactoring the initialization of the Logger

--- src/Waiter/Logging/Logger.cs
+++ src/Waiter/Logging/Logger.cs
@@ -6,6 +6,16 @@ using Waiter.WaiterClient;
 
 namespace Waiter.Logging {
     internal static class Logger {
+
+        private static CommandLineOptions _options;
+
+        private static CommandLineOptions Options {
+            get { return _options ?? ( _options = CommandLineOptions.Global ); }
+        }
+
+        internal static void Initialize( CommandLineOptions options ) {
+            _options = options;
+        }
         
         internal static void Info( string msg, params object[] args ) {
             Console.WriteLine( "INFO: {0}", string.Format( msg, args ) );
@@ -18,23 +28,23 @@ namespace Waiter.Logging {
 			Console.ForegroundColor = previousColor;
 		}
 
-		internal static void Request( Request request ) {
-			if( !CommandLineOptions.Global.Log ) {
-				return;
-			}
+        internal static void Request(Request request) {
+            if ( !Options.Log ) {
+                return;
+            }
 
-			var serializer = new XmlSerializer( typeof( Request ) );
-			var fileName = string.Format( "request-{0}.xml", request.Id );
-			var path = Path.Combine( CommandLineOptions.Global.LogDirectory, fileName );
+            var serializer = new XmlSerializer( typeof (Request) );
+            var fileName = string.Format( "request-{0}.xml", request.Id );
+            var path = Path.Combine( Options.LogDirectory, fileName );
 
-			if( !Directory.Exists( CommandLineOptions.Global.LogDirectory ) )
-				Directory.CreateDirectory( CommandLineOptions.Global.LogDirectory );
+            if ( !Directory.Exists( Options.LogDirectory ) )
+                Directory.CreateDirectory( Options.LogDirectory );
 
-			using( var stream = File.OpenWrite( path ) ) {
-				serializer.Serialize( stream, request );
-			}
-			Info( "Logged request to {0}", path );
-		}  
-  
+            using ( var stream = File.OpenWrite( path ) ) {
+                serializer.Serialize( stream, request );
+            }
+            Info( "Logged request to {0}", path );
+        }
+
     }
 }
