6211fb9468b641af462840424b7642fb27f94e19
Saquib Mian <saquib.mian@gmail.com>
2013-12-15 21:39:50 -0500

    * Forgot the commit the Request file

---
+++ src/Waiter/WaiterClient/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Waiter.WaiterClient {
+    public class Request {
+
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Method { get; set; }
+        public string Content { get; set; }
+
+    }
+}
